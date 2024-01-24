669c0a96c6edf8968b4b629d28f9e0738fbf5ff4
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-24 13:26:23 -0600

    factoring code

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
-import "./styles/Home.css"
+import "./styles/Home.css";
 
 export default function Home() {
   return (
     <main className="main-container">
-      <Link className="link" to="/KyhosCrusher">KyhosCrusher</Link>
-      <Link className="link" to="/Eckry">Eckry</Link>
-      <Link className="link" to="/Wyoming">Wyoming</Link>
+      <Link className="link" to="/KyhosCrusher">
+        KyhosCrusher
+      </Link>
+      <Link className="link" to="/Eckry">
+        Eckry
+      </Link>
+      <Link className="link" to="/Wyoming">
+        Wyoming
+      </Link>
     </main>
   );
 }
