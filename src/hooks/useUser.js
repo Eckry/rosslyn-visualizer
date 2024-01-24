ff54307c497659ca1efea6989c8e8237ebfbf671
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-24 17:54:37 -0600

    add comments

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -24,25 +24,27 @@ export default function useUser(user) {
             const problemYear = days[problemDate.getDay()];
             const problemWeek = getWeekOfMonth(problemDate);
             if (dates[problemWeek]) {
+              // Check if the week exists already
               dates[problemWeek].max = Math.max(
-                dates[problemWeek].max,
+                dates[problemWeek].max, // Find max rating of each week every iteration
                 problem.problem.rating
               );
               if (
                 dates[problemWeek][problemYear] &&
                 !dates[problemWeek][problemYear].some(
+                  //Check if the date already exists in the week and check if the problem is already there
                   (element) => element.name === problem.problem.name
                 )
               )
                 return dates[problemWeek][problemYear].push({
                   name: problem.problem.name,
-                  tags: problem.problem.tags,
+                  tags: problem.problem.tags, // If the date exists, then add new problem
                   rating: problem.problem.rating,
                 });
               return (dates[problemWeek][problemYear] = [
                 {
                   name: problem.problem.name,
-                  tags: problem.problem.tags,
+                  tags: problem.problem.tags, //If the date doesn't exists, add an array with the problem
                   rating: problem.problem.rating,
                 },
               ]);
@@ -51,7 +53,7 @@ export default function useUser(user) {
             dates[problemWeek][problemYear] = [
               {
                 name: problem.problem.name,
-                tags: problem.problem.tags,
+                tags: problem.problem.tags, // If the week doesn't exists, add it and add max rating as a property
                 rating: problem.problem.rating,
               },
             ];
