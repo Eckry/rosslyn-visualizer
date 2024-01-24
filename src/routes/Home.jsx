903c116f2d8dae6d056cbdf223b156be9f5783d8
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-23 20:22:43 -0600

    add home page

---
+++ src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "./styles/Home.css"
+
+export default function Home() {
+  return (
+    <main className="main-container">
+      <Link className="link" to="/KyhosCrusher">KyhosCrusher</Link>
+      <Link className="link" to="/Eckry">Eckry</Link>
+      <Link className="link" to="/Wyoming">Wyoming</Link>
+    </main>
+  );
+}
