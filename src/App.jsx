4085f38c9836a553e6cab35b3242d42a1f3a2a07
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-23 20:22:20 -0600

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
 import User from "./components/User";
+import Home from "./routes/Home";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/:user",
+    element: <User />
+  }
+])
 
 function App() {
   return (
-    <main className="main-container">
-      <User />
-    </main>
+    <RouterProvider router={router} />
   );
 }
 
