bf6a17ff09327fec672e63a1b30f8a26014d06a0
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-24 02:02:19 -0600

    fix classnames

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,16 +2,16 @@ import "./styles/Loading.css";
 
 export default function Loading() {
   return (
-    <div class="banter-loader">
-      <div class="banter-loader__box"></div>
-      <div class="banter-loader__box"></div>
-      <div class="banter-loader__box"></div>
-      <div class="banter-loader__box"></div>
-      <div class="banter-loader__box"></div>
-      <div class="banter-loader__box"></div>
-      <div class="banter-loader__box"></div>
-      <div class="banter-loader__box"></div>
-      <div class="banter-loader__box"></div>
+    <div className="banter-loader">
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
+      <div className="banter-loader__box"></div>
     </div>
   );
 }
