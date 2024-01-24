b26ae97ea8c7675dfa2f10920bd5abd699dab9e4
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-23 20:44:39 -0600

    add loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,17 @@
+import "./styles/Loading.css";
+
+export default function Loading() {
+  return (
+    <div class="banter-loader">
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+      <div class="banter-loader__box"></div>
+    </div>
+  );
+}
--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -3,13 +3,14 @@ import constants from "../constants.json";
 
 export default function useUser(user) {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setIsLoading(true);
     fetch(`https://codeforces.com/api/user.status?handle=${user}`)
       .then((res) => res.json())
       .then((userData) => {
         const newUserData = userData.result.reduce((userProblems, problem) => {
-          if (constants.solved[user][problem.problem.name])
-            return userProblems;
+          if (constants.solved[user][problem.problem.name]) return userProblems;
           if (
             problem.verdict === "OK" &&
             !userProblems.some((y) => y.problem.name === problem.problem.name)
@@ -19,8 +20,11 @@ export default function useUser(user) {
         }, []);
         setData(newUserData);
       })
-      .catch((e) => console.error(e));
+      .catch((e) => console.error(e))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
-  return [data];
+  return [data, isLoading];
 }
