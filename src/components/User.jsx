48053bdd2b23eedeea35be1840cd95fc659fc556
Eckry <112786854+Eckry@users.noreply.github.com>
2024-01-30 20:48:10 -0600

    add navbar

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -28,67 +28,80 @@ export default function User() {
   };
 
   return (
-    <main className="users-container">
-      <h1>{user}</h1>
-      <ul className="problems-container">
+    <>
+      <header className="user-header">
         {Object.keys(data).map((date) => {
-          let counter = 0;
           return (
-            <React.Fragment key={date}>
-              <li className="week-container">
-                <h1>Semana {Number(date) + 1}</h1>
-                <div className="days-package">
-                  {days.map((day) => {
-                    if (!data[date][day]) return <NoProblems day={day} />;
-                    return (
-                      <div key={day} className="day-container">
-                        <h4 className="day">{day}</h4>
-                        <div className="problem-container">
-                          {data[date][day].map((problem) => {
-                            counter++;
-                            const { tags, name, rating, id, index } = problem;
-                            return (
-                              <a
-                                target="_blank"
-                                href={`https://codeforces.com/problemset/problem/${id}/${index}`}
-                                className="problem"
-                                key={name}
-                              >
-                                <p className="problem-title">{name}</p>
-                                <p
-                                  style={dynamicStyleRating(
-                                    rating,
-                                    data[date].max
-                                  )}
-                                >
-                                  {rating}
-                                </p>
-                                <div className="tags-container">
-                                  {tags.map((tag) => (
-                                    <p title={tag} className="tag" key={tag}>
-                                      {tag}
-                                    </p>
-                                  ))}
-                                </div>
-                              </a>
-                            );
-                          })}
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
-                <h4>Total de semana: {counter}</h4>
-              </li>
-              <hr />
-            </React.Fragment>
+            <a className="header-link" href={`#${Number(date) + 1}`}>
+              Semana {Number(date) + 1}
+            </a>
           );
         })}
-      </ul>
+      </header>
+      <main className="users-container">
+        <h1>{user}</h1>
+        <ul className="problems-container">
+          {Object.keys(data).map((date) => {
+            let counter = 0;
+            return (
+              <React.Fragment key={date}>
+                <li className="week-container">
+                  <h1 className="week-title" id={Number(date) + 1}>
+                    Semana {Number(date) + 1}
+                  </h1>
+                  <div className="days-package">
+                    {days.map((day) => {
+                      if (!data[date][day]) return <NoProblems day={day} />;
+                      return (
+                        <div key={day} className="day-container">
+                          <h4 className="day">{day}</h4>
+                          <div className="problem-container">
+                            {data[date][day].map((problem) => {
+                              counter++;
+                              const { tags, name, rating, id, index } = problem;
+                              return (
+                                <a
+                                  target="_blank"
+                                  href={`https://codeforces.com/problemset/problem/${id}/${index}`}
+                                  className="problem"
+                                  key={name}
+                                >
+                                  <p className="problem-title">{name}</p>
+                                  <p
+                                    style={dynamicStyleRating(
+                                      rating,
+                                      data[date].max
+                                    )}
+                                  >
+                                    {rating}
+                                  </p>
+                                  <div className="tags-container">
+                                    {tags.map((tag) => (
+                                      <p title={tag} className="tag" key={tag}>
+                                        {tag}
+                                      </p>
+                                    ))}
+                                  </div>
+                                </a>
+                              );
+                            })}
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <h4>Total de semana: {counter}</h4>
+                </li>
+                <hr />
+              </React.Fragment>
+            );
+          })}
+        </ul>
 
-      <Link className="go-back" to={"/"}>
-        Volver
-      </Link>
-    </main>
+        <Link className="go-back" to={"/"}>
+          Volver
+        </Link>
+      </main>
+    </>
   );
 }
