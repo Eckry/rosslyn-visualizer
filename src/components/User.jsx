ebc982d1c580d3206092a9702c88b666034f1e37
Eckry <112786854+Eckry@users.noreply.github.com>
2024-02-10 22:14:22 -0600

    add images to home page

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -39,6 +39,7 @@ export default function User() {
         })}
       </header>
       <main className="users-container">
+        <img src="kyhos.jpeg" alt="" />
         <h1 className="profile-link">
           <a
             className="profile-anchor"
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,13 +4,13 @@ import "./styles/Home.css";
 export default function Home() {
   return (
     <main className="main-container">
-      <Link className="link" to="/KyhosCrusher">
-        KyhosCrusher
-      </Link>
-      <Link className="link" to="/Eckry">
+      <Link className="link erick" to="/Eckry">
         Eckry
       </Link>
-      <Link className="link" to="/Wyoming">
+      <Link className="link estebanP" to="/KyhosCrusher">
+        KyhosCrusher
+      </Link>
+      <Link className="link estebanS" to="/Wyoming">
         Wyoming
       </Link>
     </main>
