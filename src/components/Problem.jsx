63dc4b23c0742815ff74cc95a04a27155d7a76af
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-25 22:47:29 -0600

    add tag icon

--- src/components/Problem.jsx
+++ src/components/Problem.jsx
@@ -1,4 +1,5 @@
-import "./styles/Problem.css"
+import { IconTag } from "../icons";
+import "./styles/Problem.css";
 
 const dynamicStyleRating = (rating, max) => {
   return { color: rating === max ? "var(--highlight-color-two)" : "white" };
@@ -16,12 +17,15 @@ export default function Problem({ tags, name, rating, id, index, max }) {
     >
       <p className="problem-title">{name}</p>
       <p style={dynamicStyleRating(rating, max)}>{rating}</p>
-      <div className="tags-container">
-        {tags.map((tag, idx) => (
-          <p title={tag} className="tag" key={idx}>
-            {tag}
-          </p>
-        ))}
+      <div className="tags-show">
+        <IconTag />
+        <div className="tags-container">
+          {tags.map((tag, idx) => (
+            <p title={tag} className="tag" key={idx}>
+              {tag}
+            </p>
+          ))}
+        </div>
       </div>
     </a>
   );
