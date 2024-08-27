33cb419b1162a33799f3c6f6376e8b2b248437cd
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-26 20:45:13 -0600

    add particles to best problem solved

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -7,7 +7,7 @@ export default function Day({ data, day, max }) {
       <h4 className="day">{day}</h4>
       <div className="problem-container">
         {data.map((problem, idx) => {
-          return <Problem key={idx} {...problem} max={max} />;
+          return <Problem key={`${idx}-${problem.id}`} {...problem} max={max} />;
         })}
       </div>
     </div>
--- src/components/Problem.jsx
+++ src/components/Problem.jsx
@@ -1,6 +1,7 @@
 import { IconTag } from "../icons";
 import { colors } from "../utils";
 import "./styles/Problem.css";
+import Particles from "@tsparticles/react";
 
 const dynamicStyleRating = (rating) => {
   if (rating >= 2400) return { color: colors.grandmaster };
@@ -12,15 +13,33 @@ const dynamicStyleRating = (rating) => {
   return { color: colors.newbie };
 };
 
-export default function Problem({ tags, name, rating, id, index }) {
+export default function Problem({ tags, name, rating, id, index, max }) {
+  const options = {
+    preset: "stars",
+    background: { color: "transparent" },
+    fullScreen: { enable: false },
+    particles: {
+      number: { value: 10 },
+      size: { value: 2 },
+      color: { value: "#facc15" },
+    },
+  };
+
+  const showParticles = max === rating;
   return (
     <a
       target="_blank"
       href={`https://codeforces.com/problemset/problem/${id}/${index}`}
       className="problem"
-      key={name}
       rel="noreferrer"
     >
+      {showParticles && (
+        <Particles
+          options={options}
+          id={`${index}-${id}`}
+          className="tsparticles2"
+        />
+      )}
       <p className="problem-title">{name}</p>
       <p style={dynamicStyleRating(rating)}>{rating}</p>
       <div className="tags-show">
