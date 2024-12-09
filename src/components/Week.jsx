038db5241694f588263d98316ad686ef56bd4537
Eckry <112786854+Eckry@users.noreply.github.com>
2024-12-08 20:10:40 -0600

    add styles to problems opener

--- src/components/Week.jsx
+++ src/components/Week.jsx
@@ -3,6 +3,11 @@ import Day from "./Day";
 import { days } from "../constants.json";
 
 export default function Week({ date, data }) {
+  const totalSolved = Object.keys(data[date]).reduce((acc, day) => {
+    if (day === "max") return acc;
+    return acc + data[date][day].length;
+  }, 0);
+
   return (
     <>
       <li className="week-container">
@@ -10,8 +15,17 @@ export default function Week({ date, data }) {
           <span className="week-highlight">{Number(date) + 1}.</span>
           <span className="week-line"></span>
         </h1>
-        <label htmlFor={`days-opener-${Number(date)}`} className="days-opener-button"></label>
-        <input id={`days-opener-${Number(date)}`} className="days-opener" type="checkbox" />
+        <label
+          htmlFor={`days-opener-${Number(date)}`}
+          className="days-opener-button"
+        >
+          {totalSolved}
+        </label>
+        <input
+          id={`days-opener-${Number(date)}`}
+          className="days-opener"
+          type="checkbox"
+        />
         <div className="days-package">
           {days.map((day, idx) => {
             if (!data[date][day]) return;
