1350c038a8015c5035a51a988debf017766a23c2
Eckry <112786854+Eckry@users.noreply.github.com>
2024-12-10 20:38:16 -0600

    add dynamic color to day title

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,13 +1,19 @@
-import "./styles/Day.css"
+import "./styles/Day.css";
 import Problem from "./Problem";
+import { dynamicStyleRating } from "../utils";
 
 export default function Day({ data, day, max }) {
+  const color = dynamicStyleRating(max);
   return (
     <div key={day} className="day-container">
-      <h4 className="day">{day}</h4>
+      <h4 className="day" style={{ color }}>
+        {day}
+      </h4>
       <div className="problem-container">
         {data.map((problem, idx) => {
-          return <Problem key={`${idx}-${problem.id}`} {...problem} max={max} />;
+          return (
+            <Problem key={`${idx}-${problem.id}`} {...problem} max={max} />
+          );
         })}
       </div>
     </div>
