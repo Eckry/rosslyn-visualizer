ab81ea32310c81d09937491be7b2c01819f5b4a8
Eckry <112786854+Eckry@users.noreply.github.com>
2024-12-10 20:34:59 -0600

    update colors

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,21 @@
 export const colors = {
-  newbie: "rgb(178, 178, 178)",
-  pupil: "rgb(82, 255, 186)",
-  specialist: "#32ceff",
-  expert: "#001bff",
-  candidate: "#C74EAD",
-  master: "#FFA726",
-  grandmaster: "#FF1208",
+  newbie: "#808080",
+  pupil: "#99D342",
+  apprentice: "#50A750",
+  specialist: "#03A89E",
+  expert: "#0000FF",
+  candidate: "#AA00AA",
+  master: "#FE9516",
+  grandmaster: "#FF0000",
 };
 
 export const dynamicStyleRating = (rating) => {
   if (rating >= 2400) return colors.grandmaster;
-  if (rating >= 2100) return colors.master;
-  if (rating >= 1900) return colors.candidate;
+  if (rating >= 2000) return colors.master;
+  if (rating >= 1800) return colors.candidate;
   if (rating >= 1600) return colors.expert;
   if (rating >= 1400) return colors.specialist;
-  if (rating >= 1200) return colors.pupil;
+  if (rating >= 1200) return colors.apprentice;
+  if (rating >= 1000) return colors.pupil;
   return colors.newbie;
 };
