6e1dfb7eb12e6bbd29e582ab5e9e40168c34bd42
Eckry <112786854+Eckry@users.noreply.github.com>
2024-12-11 18:27:10 -0600

    add dynamic color for day name

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -3,10 +3,15 @@ import Problem from "./Problem";
 import { dynamicStyleRating } from "../utils";
 
 export default function Day({ data, day, max }) {
-  const color = dynamicStyleRating(max);
+  const maxRating = data.reduce((acc, problem) => {
+    return acc < problem.rating ? problem.rating : acc;
+  }, 0);
+
+  const dayNameColor = dynamicStyleRating(maxRating);
+  
   return (
     <div key={day} className="day-container">
-      <h4 className="day" style={{ color }}>
+      <h4 className="day" style={{ color: dayNameColor }}>
         {day}
       </h4>
       <div className="problem-container">
