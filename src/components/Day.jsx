2fb9481bdec75b25a557abecb21509b8d137e6a3
Eckry <112786854+Eckry@users.noreply.github.com>
2024-12-24 16:32:53 -0600

    add new colors to week title

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,19 +1,10 @@
 import "./styles/Day.css";
 import Problem from "./Problem";
-import { dynamicStyleRating } from "../utils";
 
 export default function Day({ data, day, max }) {
-  const maxRating = data.reduce((acc, problem) => {
-    return acc < problem.rating ? problem.rating : acc;
-  }, 0);
-
-  const {light: dayNameColor} = dynamicStyleRating(maxRating);
-  
   return (
     <div key={day} className="day-container">
-      <h4 className="day" style={{ color: dayNameColor }}>
-        {day}
-      </h4>
+      <h4 className="day">{day}</h4>
       <div className="problem-container">
         {data.map((problem, idx) => {
           return (
